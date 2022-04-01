87f62271162da7a0df1868c981488fa67e511a13
adam <ikaneadam@gmail.com>
2022-04-01 14:33:59 +0200

    connect db with heroku

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,15 +6,17 @@ import {Message} from "./entity/Message";
 
 
 export const AppDataSource = new DataSource({
+    url:"postgres://jaegicykdpdivx:d1d6844169950cc1986d6fba4812ca5c97dd8869646ad3f6f3319f64c39cf3c6@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7l7kk1pks5er5",
     type: "postgres",
-    host: "db-persist",
-    port: 5432,
-    username: "postgres",
-    password: "password",
-    database: "postgres",
+    ssl: true,
+    extra: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    },
     synchronize: true,
     logging: false,
-    entities: [User, Chat, Message],
-    migrations: [],
-    subscribers: [],
+    entities: [User, Chat, Message]
 })
+
+// postgres://jaegicykdpdivx:d1d6844169950cc1986d6fba4812ca5c97dd8869646ad3f6f3319f64c39cf3c6@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7l7kk1pks5er5
