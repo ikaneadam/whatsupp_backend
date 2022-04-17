80a45400b9f11d3c83c6effed35231e2430954a5
adam <ikaneadam07@gmail.com>
2022-04-17 21:39:10 +0200

    refactor sockets

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,13 +7,10 @@ import {Message} from "./entity/Message";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url:"postgres://jaegicykdpdivx:d1d6844169950cc1986d6fba4812ca5c97dd8869646ad3f6f3319f64c39cf3c6@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d7l7kk1pks5er5",
-    ssl: true,
-    extra: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    },
+    host: "db-persist",
+    username: "postgres",
+    password: "password",
+    database: "postgres",
     port: 5432,
     synchronize: true,
     logging: false,
